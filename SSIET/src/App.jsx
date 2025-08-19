528c025276816f830083608169de462321d96647
jyotismita-das-365 <jyotismitadas365@gmail.com>
2025-08-19 21:16:09 +0530

    Start creating web page

--- SSIET/src/App.jsx
+++ SSIET/src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer"
 import Navbar from "./components/navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   )
 }
---
+++ SSIET/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  
+  return (
+    <>
+    <div class="bg-red-700 text-white">
+      <p>Affiliated & Approved By AICTE, DCI, IKGPTU, BFUHS, PSBTE, INC, CBSE, BCI & PCI and Govt. of Punjab</p>
+      <p>CAMPUS: 8 KM’s FROM CHANDIGARH</p>
+      <p>Important Links</p>
+      <p>Chandigarh-Delhi NH-152, Dera Bassi, (Punjab)</p>
+      <a href="https://www.srisukhmanigroup.edu.in/">SSGI</a>
+      <a href="https://swayam.gov.in/">Swayam</a>
+      <h1>Contact Information</h1>
+      <h2>Phone no: +91 8765756687</h2>
+
+      <h1>Copyright ©2025 Sri Sukhmani Institute of Engineering & Technology | <a href="#">Disclaimer</a> <a href="#">Privacy Policy</a></h1>
+      <a href="#">Instagram</a>
+      <a href="#">Facebook</a>
+      <a href="#">Youtube</a>
+    </div>
+    </>
+  )
+};
+
+export default Footer;
